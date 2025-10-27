a6c5448fdc564f8423c64fa2a0a16a88265a94b0
moraissdev <renanbusinessdev@gmail.com>
2025-10-27 19:35:51 -0300

    commit

--- main.c
+++ main.c
@@ -29,57 +29,64 @@ void buscaEmLargura_labirinto(char **labirinto, int **volumeSala, int dimensaoGr
 int main() {
     int dimensaoGrid;
     fila filaLabirinto;
+
     scanf("%d\n", &dimensaoGrid);
-    char **labirinto = malloc(sizeof(char*) * dimensaoGrid); //aloca na heap memoria para a matriz do labirinto
-    int **volumeSala = malloc(sizeof(int*) * dimensaoGrid); //aloca na heap memoria para o volume de cada sala ate a saida
+
+    char **labirinto = malloc(sizeof(char*) * dimensaoGrid); //aloca na heap memoria para as linhas do labirinto (vetor de ponteiros)
+    int **volumeSala = malloc(sizeof(int*) * dimensaoGrid); //aloca na heap memoria para o volume das salas (vetor de ponteiros)
+
     for (int y = 0; y < dimensaoGrid; y++) {
-        labirinto[y] = malloc(sizeof(char) * dimensaoGrid);
-        volumeSala[y] = malloc(sizeof(int) * dimensaoGrid);
+        labirinto[y] = malloc(sizeof(char) * dimensaoGrid); //aloca na heap memoria paras as colunas de cada linha do labirinto
+        volumeSala[y] = malloc(sizeof(int) * dimensaoGrid); //aloca na heap memoria para as colunas de cada lnha do volume das salas
     }
-    for (int y = 0; y < dimensaoGrid; y++) {
-        for (int x = 0; x < dimensaoGrid; x++) {
-            scanf("%c", &labirinto[y][x]);
-            volumeSala[y][x] = dimensaoGrid * dimensaoGrid;
+
+    for (int y = 0; y < dimensaoGrid; y++) { ///percorre as linhas da matriz
+        for (int x = 0; x < dimensaoGrid; x++) { //percorre as colunas da matriz
+            scanf("%c", &labirinto[y][x]); //le e atribui os caracteres a cada ponto (X ou 0)
+            volumeSala[y][x] = dimensaoGrid * dimensaoGrid; //inicializa cada ponto com a distancia sendo n² (infinita)
         }
         getchar();
     }
+
     buscaEmLargura_labirinto(labirinto, volumeSala, dimensaoGrid);
+
     for (int y = 0; y < dimensaoGrid; y++) {
         for (int x = 0; x < dimensaoGrid; x++) {
             printf("%d ", volumeSala[y][x]);
         } printf("\n");
     }
-    for (int i = 0; i < dimensaoGrid; i++) {
-        free(labirinto[i]);
-        free(volumeSala[i]);
+
+    for (int i = 0; i < dimensaoGrid; i++) { //for para liberar a memória da heap
+        free(labirinto[i]); //libera memoria das linhas do labirinto
+        free(volumeSala[i]); //libera memoria das linhas do volume das salas
     }
-    free(labirinto);
-    free(volumeSala);
+    free(labirinto); //libera a "cabeca" do labirinto
+    free(volumeSala); //libera a "cabeca" do volume das salas
     return 0;
 }
 
 fila criar_fila(int tamanhoFila) {
-    fila filaLabirinto = malloc(sizeof(struct fila));
-    filaLabirinto->pontos = malloc(sizeof(ponto) * tamanhoFila);
-    filaLabirinto->inicio = 0;
-    filaLabirinto->final = 0;
+    fila filaLabirinto = malloc(sizeof(struct fila)); //aloca na heap memoria para uma struct da fila
+    filaLabirinto->pontos = malloc(sizeof(ponto) * tamanhoFila); //aloca na heap um vetor para os pontos do labirinto
+    filaLabirinto->inicio = 0; //inicializa inicio da fila
+    filaLabirinto->final = 0; //inicializa final da fila
     return filaLabirinto;
 }
 
 void inserir_fila(fila filaLabirinto, ponto p, int tamanhoFila) {
-    if ((filaLabirinto->final+1) % (tamanhoFila) == filaLabirinto->inicio) {
+    if ((filaLabirinto->final+1) % (tamanhoFila) == filaLabirinto->inicio) { //verifica se a fila ta cheia
         return;
     }
-    filaLabirinto->pontos[filaLabirinto->final] = p;
+    filaLabirinto->pontos[filaLabirinto->final] = p; //se nao estiver vazia, coloco o ponto no final dela
     filaLabirinto->final = filaLabirinto->final + 1;
     if (filaLabirinto->final == tamanhoFila) {
-        filaLabirinto->final = 0;
+        filaLabirinto->final = 0; 
     }
 }
 
 ponto remover_fila(fila filaLabirinto, int tamanhoFila) {
-    ponto p = filaLabirinto->pontos[filaLabirinto->inicio];
-    if (filaLabirinto->inicio == filaLabirinto->final) {
+    ponto p = filaLabirinto->pontos[filaLabirinto->inicio]; //pega o ponto da primeira posicao da fila para remover (FIFO)
+    if (filaLabirinto->inicio == filaLabirinto->final) { //verifica se a fila ta vazia, se sim, retorna o proprio ponto
         return p;
     }
     p = filaLabirinto->pontos[filaLabirinto->inicio];
@@ -103,27 +110,26 @@ void liberar_fila(fila filaLabirinto) {
 }
 
 void buscaEmLargura_labirinto(char **labirinto, int **volumeSala, int dimensaoGrid) {
-    fila filaLabirinto = criar_fila(dimensaoGrid * dimensaoGrid);
-    if (labirinto[0][0] != 'X') {
-        ponto inicio = {0,0};
-        inserir_fila(filaLabirinto, inicio, dimensaoGrid * dimensaoGrid);
-        volumeSala[0][0] = 0;
-    }
-    int deslocamentoX[4] = {-1, 1, 0, 0};
-    int deslocamentoY[4] = {0, 0, -1 , 1};
-    while (!fila_vazia(filaLabirinto)) {
-        ponto pontoAtual = remover_fila(filaLabirinto, dimensaoGrid * dimensaoGrid);
-        for (int i = 0; i < 4; i++) {
-            int proximoX = pontoAtual.x + deslocamentoX[i];
-            int proximoY = pontoAtual.y + deslocamentoY[i];
-            if ((proximoX >= 0 && proximoX < dimensaoGrid) && (proximoY >= 0 && proximoY < dimensaoGrid)) {
-                if (labirinto[proximoY][proximoX] != 'X' && volumeSala[proximoY][proximoX] > volumeSala[pontoAtual.y][pontoAtual.x]+1) {
-                    volumeSala[proximoY][proximoX] = volumeSala[pontoAtual.y][pontoAtual.x]+1;
-                    ponto proximoPonto = {proximoX, proximoY};
-                    inserir_fila(filaLabirinto, proximoPonto, dimensaoGrid * dimensaoGrid);
+    fila filaLabirinto = criar_fila(dimensaoGrid * dimensaoGrid); //cria uma fila como n², para suportar todos os pontos (matriz)
+    ponto inicio = {0,0}; //ponto inicial eh sempre (0,0)
+
+    inserir_fila(filaLabirinto, inicio, dimensaoGrid * dimensaoGrid); //coloca o ponto de inicio na fila
+    volumeSala[0][0] = 0; //distancia/volume do ponto inicial (0,0) eh sempre 0
+    ponto movimentos[4] = {{-1,0}, {1,0}, {0,-1}, {0,1}}; //array com todos os movimentos possiveis de um ponto (excete diagonais)
+    
+    while (!fila_vazia(filaLabirinto)) { //itera enquanto a fila estiver pontos ainda
+        ponto pontoAtual = remover_fila(filaLabirinto, dimensaoGrid * dimensaoGrid); //pega o prox ponto da fila para tirar (FIFO)
+        for (int i = 0; i < 4; i++) { //for para percorrer os 4 pontos vizinhos do ponto atual
+            int proximoX = pontoAtual.x + movimentos[i].x;
+            int proximoY = pontoAtual.y + movimentos[i].y;
+            if ((proximoX >= 0 && proximoX < dimensaoGrid) && (proximoY >= 0 && proximoY < dimensaoGrid)) { //verifica se o ponto vizinho está dentro do limite da matriz
+                if (labirinto[proximoY][proximoX] != 'X' && volumeSala[proximoY][proximoX] > volumeSala[pontoAtual.y][pontoAtual.x]+1) { //verifica se o ponto vizinho nn é uma sala bloqueada
+                    volumeSala[proximoY][proximoX] = volumeSala[pontoAtual.y][pontoAtual.x]+1; //muda o valor do ponto vizinho para a menor distancia 
+                    ponto proximoPonto = {proximoX, proximoY}; //cria o ponto vizinho
+                    inserir_fila(filaLabirinto, proximoPonto, dimensaoGrid * dimensaoGrid); //insere o ponto vizinho na fila
                 }
             }
         }
     }
-    liberar_fila(filaLabirinto);
+    liberar_fila(filaLabirinto); //libera a memoria na heap da fila dps de fazer a busca em largura
 }
